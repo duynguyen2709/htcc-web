46a51a183779be667316a13abf891f78f61e3207
Dat Nguyen <nguyenquocdat2511998@gmail.com>
2020-03-08 16:21:11 +0700

    first push branch

--- upgrade_vuetify/nuxt.config.js
+++ upgrade_vuetify/nuxt.config.js
@@ -14,7 +14,10 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
 
-    link: [ {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}, {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'} ],
+    link: [ {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}, 
+          {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'}, 
+          { rel: 'stylesheet', href: '/vuetify.min.css' }
+        ],
   
 
 
@@ -33,7 +36,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '~/plugins/vuetify'
+    //'~/plugins/vuetify'
   ],
   /*
   ** Nuxt.js dev-modules
