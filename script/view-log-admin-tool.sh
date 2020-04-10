82d518fb3f12127bfbe444a6173cd65bb54263a5
duyna5 <duyna5@vng.com.vn>
2020-04-10 11:47:02 +0700

    test generate

--- script/view-log-admin-tool.sh
+++ script/view-log-admin-tool.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 password=d8B*.61ouL3Hgz!=
-sshpass -p $password ssh -o StrictHostKeyChecking=no -t -t root@108.61.162.225
-docker logs -f htcc-admin-tool
+sshpass -p $password ssh -o StrictHostKeyChecking=no -t -t root@108.61.162.225 "docker logs -f htcc-admin-tool"
