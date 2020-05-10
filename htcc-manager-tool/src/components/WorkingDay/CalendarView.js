6c262f9fa69eb33b00607c33ace8271b5d36ee0c
duyna5 <duyna5@vng.com.vn>
2020-05-10 11:15:31 +0700

    fix loader

--- htcc-manager-tool/src/components/WorkingDay/CalendarView.js
+++ htcc-manager-tool/src/components/WorkingDay/CalendarView.js
@@ -6,7 +6,7 @@ import {workScheduleApi} from '../../api';
 import {store} from 'react-notifications-component';
 import {createNotify} from '../../utils/notifier';
 import * as _ from 'lodash';
-import Loading from "../Loader/Loading";
+import ReactLoading from "react-loading";
 
 class CalendarView extends Component {
     constructor(props) {
@@ -142,113 +142,117 @@ class CalendarView extends Component {
         const {optionsOffices} = this.props;
         const {isLoading, currentOffices} = this.state;
 
-        if (isLoading) {
-            return <Loading />
-        }
-
         return (
             <Row>
-                <Calendar
-                    dateCellRender={this.dateCellRender}
-                    headerRender={({value, type, onChange, onTypeChange}) => {
-                        const start = 0;
-                        const end = 12;
-                        const monthOptions = [];
+                {isLoading ?
+                    <ReactLoading
+                        type={'spinningBubbles'}
+                        color={'#4caf50'}
+                        style={{position: 'relative', left: '45%'}}
+                        height={'10%'}
+                        width={'10%'}
+                    /> :
+                    <Calendar
+                        dateCellRender={this.dateCellRender}
+                        headerRender={({value, type, onChange, onTypeChange}) => {
+                            const start = 0;
+                            const end = 12;
+                            const monthOptions = [];
 
-                        const current = value.clone();
-                        const localeData = value.localeData();
-                        const months = [];
-                        for (let i = 0; i < 12; i++) {
-                            current.month(i);
-                            months.push(localeData.months(current));
-                        }
+                            const current = value.clone();
+                            const localeData = value.localeData();
+                            const months = [];
+                            for (let i = 0; i < 12; i++) {
+                                current.month(i);
+                                months.push(localeData.months(current));
+                            }
 
-                        for (let index = start; index < end; index++) {
-                            monthOptions.push(
-                                <Select.Option
-                                    className="month-item"
-                                    key={`${index}`}
-                                    value={String(index + 1)}
-                                >
-                                    {months[index]}
-                                </Select.Option>
-                            );
-                        }
-                        const month = value.month();
-
-                        const year = value.year();
-                        const options = [];
-                        for (let i = year - 5; i <= year + 1; i += 1) {
-                            options.push(
-                                <Select.Option
-                                    key={i}
-                                    value={i}
-                                    className="year-item"
-                                >
-                                    {i}
-                                </Select.Option>
-                            );
-                        }
-
-                        return (
-                            <Row style={{borderBottom: '2px solid #389e0d'}}>
-                                <Col sm={4} className="mr-2">
-                                    <SelectBox
-                                        key={optionsOffices}
-                                        options={optionsOffices}
-                                        returnValue={this.getOfficeId}
-                                        currentOffices={currentOffices}
-                                    />
-                                </Col>
-                                <Col sm={2} className="mr-2">
-                                    <Select
-                                        style={{width: '100%'}}
-                                        dropdownMatchSelectWidth={false}
-                                        value={String(month)}
-                                        onChange={(selectedMonth) => {
-                                            const newValue = value.clone();
-                                            newValue.month(
-                                                parseInt(selectedMonth, 10)
-                                            );
-                                            onChange(newValue);
-                                        }}
+                            for (let index = start; index < end; index++) {
+                                monthOptions.push(
+                                    <Select.Option
+                                        className="month-item"
+                                        key={`${index}`}
+                                        value={String(index + 1)}
                                     >
-                                        {monthOptions}
-                                    </Select>
-                                </Col>
-                                <Col sm={2} className="mr-2">
-                                    <Select
-                                        style={{width: '100%'}}
-                                        dropdownMatchSelectWidth={false}
-                                        className="my-year-select"
-                                        onChange={(newYear) => {
-                                            const now = value
-                                                .clone()
-                                                .year(newYear);
-                                            onChange(now);
-                                        }}
-                                        value={String(year)}
+                                        {months[index]}
+                                    </Select.Option>
+                                );
+                            }
+                            const month = value.month();
+
+                            const year = value.year();
+                            const options = [];
+                            for (let i = year - 5; i <= year + 1; i += 1) {
+                                options.push(
+                                    <Select.Option
+                                        key={i}
+                                        value={i}
+                                        className="year-item"
                                     >
-                                        {options}
-                                    </Select>
-                                </Col>
-                                <Col offset={1} className="mr-2">
-                                    <Badge
-                                        status="success"
-                                        text="Ngày nghỉ lễ"
-                                    />
-                                    <br/>
-                                    <Badge
-                                        status="orange"
-                                        text="Ngày nghỉ thường"
-                                    />
-                                    <br/>
-                                </Col>
-                            </Row>
-                        );
-                    }}
-                    onPanelChange={this.onPanelChange}
-                />
+                                        {i}
+                                    </Select.Option>
+                                );
+                            }
+
+                            return (
+                                <Row style={{borderBottom: '2px solid #389e0d'}}>
+                                    <Col sm={4} className="mr-2">
+                                        <SelectBox
+                                            key={optionsOffices}
+                                            options={optionsOffices}
+                                            returnValue={this.getOfficeId}
+                                            currentOffices={currentOffices}
+                                        />
+                                    </Col>
+                                    <Col sm={2} className="mr-2">
+                                        <Select
+                                            style={{width: '100%'}}
+                                            dropdownMatchSelectWidth={false}
+                                            value={String(month)}
+                                            onChange={(selectedMonth) => {
+                                                const newValue = value.clone();
+                                                newValue.month(
+                                                    parseInt(selectedMonth, 10)
+                                                );
+                                                onChange(newValue);
+                                            }}
+                                        >
+                                            {monthOptions}
+                                        </Select>
+                                    </Col>
+                                    <Col sm={2} className="mr-2">
+                                        <Select
+                                            style={{width: '100%'}}
+                                            dropdownMatchSelectWidth={false}
+                                            className="my-year-select"
+                                            onChange={(newYear) => {
+                                                const now = value
+                                                    .clone()
+                                                    .year(newYear);
+                                                onChange(now);
+                                            }}
+                                            value={String(year)}
+                                        >
+                                            {options}
+                                        </Select>
+                                    </Col>
+                                    <Col offset={1} className="mr-2">
+                                        <Badge
+                                            status="success"
+                                            text="Ngày nghỉ lễ"
+                                        />
+                                        <br/>
+                                        <Badge
+                                            status="orange"
+                                            text="Ngày nghỉ thường"
+                                        />
+                                        <br/>
+                                    </Col>
+                                </Row>
+                            );
+                        }}
+                        onPanelChange={this.onPanelChange}
+                    />}
             </Row>
         );
     }
