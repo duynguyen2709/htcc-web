20babcb7001b584cc8f3664bca7e9c0d9cd8894b
Âu Trương Thị Hải <autth@vng.com.vn>
2020-06-12 15:18:55 +0700

    update nabar

--- htcc-manager-tool/src/layouts/Admin.js
+++ htcc-manager-tool/src/layouts/Admin.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import PerfectScrollbar from 'perfect-scrollbar';
 import * as _ from 'lodash';
 import Navbar from '../components/Navbars/Navbar';
