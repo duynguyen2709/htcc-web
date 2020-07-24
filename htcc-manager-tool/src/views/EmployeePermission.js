d914778cc99557a11eb80dd382e7e2be6c078363
Âu Trương Thị Hải <autth@vng.com.vn>
2020-07-24 10:36:34 +0700

    clean code

--- htcc-manager-tool/src/views/EmployeePermission.js
+++ htcc-manager-tool/src/views/EmployeePermission.js
@@ -1,15 +1,18 @@
-import React, {Component} from 'react';
-import {Card, Col, Empty, List, Popconfirm, Row} from 'antd';
-import {connect} from 'react-redux';
-import {permissionApi} from "../api";
-import {store} from 'react-notifications-component';
-import {createNotify} from "../utils/notifier";
-import ReactLoading from "react-loading";
-import {CheckCircleOutlined, EditOutlined, QuestionCircleOutlined} from '@ant-design/icons';
-import {Button, CardFooter} from 'reactstrap';
-import EmployeePermissionCard from "../components/Permission/EmployeePermissionCard";
-import {USER} from "../constant/localStorageKey";
-import * as _ from "lodash";
+import React, { Component } from 'react';
+import { Card, Col, Empty, List, Popconfirm, Row } from 'antd';
+import { connect } from 'react-redux';
+import { permissionApi } from '../api';
+import { store } from 'react-notifications-component';
+import { createNotify } from '../utils/notifier';
+import ReactLoading from 'react-loading';
+import {
+    CheckCircleOutlined,
+    EditOutlined,
+    QuestionCircleOutlined,
+} from '@ant-design/icons';
+import { Button, CardFooter } from 'reactstrap';
+import EmployeePermissionCard from '../components/Permission/EmployeePermissionCard';
+import * as _ from 'lodash';
 
 class EmployeePermission extends Component {
     constructor(props) {
@@ -19,8 +22,8 @@ class EmployeePermission extends Component {
             data: null,
             currentUsername: '',
             isLoading: false,
-            readOnly: true
-        }
+            readOnly: true,
+        };
     }
 
     componentDidMount() {
@@ -28,7 +31,7 @@ class EmployeePermission extends Component {
     }
 
     getConfig = () => {
-        this.setState({isLoading: true});
+        this.setState({ isLoading: true });
         permissionApi
             .getEmployeePermission('duytv')
             // .getEmployeePermission(this.props.username)
@@ -36,7 +39,7 @@ class EmployeePermission extends Component {
                 if (res.returnCode === 1) {
                     console.log(res.data);
                     this.setState({
-                        data: res.data
+                        data: res.data,
                     });
                 } else {
                     store.addNotification(
@@ -50,12 +53,12 @@ class EmployeePermission extends Component {
                 );
             })
             .finally(() => {
-                this.setState({isLoading: false});
+                this.setState({ isLoading: false });
             });
     };
 
     renderButton = () => {
-        const {readOnly} = this.state;
+        const { readOnly } = this.state;
 
         if (readOnly) {
             return (
@@ -66,7 +69,9 @@ class EmployeePermission extends Component {
                     color="primary"
                     type="button"
                 >
-                    <EditOutlined style={{display: 'inline', margin: '5px 10px 0 0'}}/>{' '}
+                    <EditOutlined
+                        style={{ display: 'inline', margin: '5px 10px 0 0' }}
+                    />{' '}
                     {'  '}
                     <span className="btn-save-text"> Chỉnh sửa</span>
                 </Button>
@@ -76,7 +81,7 @@ class EmployeePermission extends Component {
         return (
             <Popconfirm
                 title="Bạn chắc chắn thay đổi？"
-                icon={<QuestionCircleOutlined/>}
+                icon={<QuestionCircleOutlined />}
                 okText="Đồng ý"
                 cancelText="Huỷ"
                 // onConfirm={() => this.handleSubmit()}
@@ -88,7 +93,7 @@ class EmployeePermission extends Component {
                     type="button"
                 >
                     <CheckCircleOutlined
-                        style={{display: 'inline', margin: '5px 10px 0 0'}}
+                        style={{ display: 'inline', margin: '5px 10px 0 0' }}
                     />{' '}
                     {'  '}
                     <span className="btn-save-text"> LƯU</span>
@@ -108,14 +113,19 @@ class EmployeePermission extends Component {
         if (list.length === 3) {
             span = 8;
         }
-        return <Row>
-            {_.map(list, (item, index) => (
-                <Col span={span} style={{padding: 10}}
-                     key={item.username}>
-                    <EmployeePermissionCard user={item}/>
-                </Col>
-            ))}
-        </Row>
+        return (
+            <Row>
+                {_.map(list, (item, index) => (
+                    <Col
+                        span={span}
+                        style={{ padding: 10 }}
+                        key={item.username}
+                    >
+                        <EmployeePermissionCard user={item} />
+                    </Col>
+                ))}
+            </Row>
+        );
     };
 
     renderListSubManager = (list) => {
@@ -129,20 +139,19 @@ class EmployeePermission extends Component {
         if (list.length === 3) {
             span = 8;
         }
-        return <Row>
-            {_.map(list, (item, index) => (
-                <Col span={span} key={item.username}>
-                    <EmployeePermissionCard user={item}/>
-                </Col>
-            ))}
-        </Row>
+        return (
+            <Row>
+                {_.map(list, (item, index) => (
+                    <Col span={span} key={item.username}>
+                        <EmployeePermissionCard user={item} />
+                    </Col>
+                ))}
+            </Row>
+        );
     };
 
     render() {
-        const {
-            isLoading,
-            data
-        } = this.state;
+        const { isLoading, data } = this.state;
 
         if (isLoading) {
             return (
@@ -160,9 +169,9 @@ class EmployeePermission extends Component {
             return (
                 <Empty
                     className={'center-div'}
-                    style={{marginTop: '20px'}}
+                    style={{ marginTop: '20px' }}
                     description={
-                        <span style={{color: 'rgba(0, 0, 0, 0.65)'}}>
+                        <span style={{ color: 'rgba(0, 0, 0, 0.65)' }}>
                             Không có dữ liệu
                         </span>
                     }
@@ -170,79 +179,123 @@ class EmployeePermission extends Component {
             );
         }
 
-        const {dataView, dataEdit} = data;
+        const { dataView } = data;
 
         return (
             <div className="content">
                 <div className="table-wrapper tabs-big">
-                    <Row className={"permission-container"}>
+                    <Row className={'permission-container'}>
                         <Col span={16}>
-                            <Row className={"permission-user-list"}>
-                                <Card title="Quyền nhân viên"
-                                      bordered={true}
-                                      className={"card-employee-permission"}
+                            <Row className={'permission-user-list'}>
+                                <Card
+                                    title="Quyền nhân viên"
+                                    bordered={true}
+                                    className={'card-employee-permission'}
                                 >
-                                    <div className={"card-employee-permission-content"}>
-                                        <Row className={"permission-manager-list"}
-                                             justify={"space-around"}
+                                    <div
+                                        className={
+                                            'card-employee-permission-content'
+                                        }
+                                    >
+                                        <Row
+                                            className={
+                                                'permission-manager-list'
+                                            }
+                                            justify={'space-around'}
                                         >
-                                            {dataView.lineManager ?
-                                                <Card type="inner" title="Cấp trên trực tiếp">
-                                                    <EmployeePermissionCard user={dataView.lineManager}/>
-                                                </Card> : null}
-                                            {!_.isEmpty(dataView.subManagers) ?
-                                                <Card type="inner" title="Quản lý phụ">
-                                                    {this.renderListSubManager(dataView.subManagers)}
-                                                </Card> : null}
+                                            {dataView.lineManager ? (
+                                                <Card
+                                                    type="inner"
+                                                    title="Cấp trên trực tiếp"
+                                                >
+                                                    <EmployeePermissionCard
+                                                        user={
+                                                            dataView.lineManager
+                                                        }
+                                                    />
+                                                </Card>
+                                            ) : null}
+                                            {!_.isEmpty(
+                                                dataView.subManagers
+                                            ) ? (
+                                                <Card
+                                                    type="inner"
+                                                    title="Quản lý phụ"
+                                                >
+                                                    {this.renderListSubManager(
+                                                        dataView.subManagers
+                                                    )}
+                                                </Card>
+                                            ) : null}
                                         </Row>
-                                        <Row className={"permission-self"}
-                                             justify={"center"}
+                                        <Row
+                                            className={'permission-self'}
+                                            justify={'center'}
                                         >
-                                            <Card type="inner" title="Cá nhân"
-                                            >
-                                                <EmployeePermissionCard user={dataView.self}/>
+                                            <Card type="inner" title="Cá nhân">
+                                                <EmployeePermissionCard
+                                                    user={dataView.self}
+                                                />
                                             </Card>
                                         </Row>
-                                        {!_.isEmpty(dataView.subordinates) ?
-                                            <Row className={"permission-subordinate-list"}
-                                                 justify={"center"}
+                                        {!_.isEmpty(dataView.subordinates) ? (
+                                            <Row
+                                                className={
+                                                    'permission-subordinate-list'
+                                                }
+                                                justify={'center'}
                                             >
-                                                <Card type="inner" title="Nhân viên dưới quyền">
-                                                    {this.renderListSubordinate(dataView.subordinates)}
+                                                <Card
+                                                    type="inner"
+                                                    title="Nhân viên dưới quyền"
+                                                >
+                                                    {this.renderListSubordinate(
+                                                        dataView.subordinates
+                                                    )}
                                                 </Card>
-                                            </Row> : null}
+                                            </Row>
+                                        ) : null}
                                     </div>
                                 </Card>
                             </Row>
                         </Col>
                         <Col span={7} offset={1}>
-                            <Row className={"permission-office-list"}>
+                            <Row className={'permission-office-list'}>
                                 <List
-                                    style={{width: '100%'}}
+                                    style={{ width: '100%' }}
                                     header={<div>Chi nhánh được quản lý</div>}
                                     bordered
                                     dataSource={dataView.canManageOffices}
-                                    renderItem={item => (
+                                    renderItem={(item) => (
                                         <List.Item>
-                                            <h4>({item.officeId}) {item.officeName}</h4>
+                                            <h4>
+                                                ({item.officeId}){' '}
+                                                {item.officeName}
+                                            </h4>
                                         </List.Item>
                                     )}
                                 />
                             </Row>
-                            <Row className={"permission-department-list"}>
+                            <Row className={'permission-department-list'}>
                                 <List
-                                    style={{width: '100%'}}
+                                    style={{ width: '100%' }}
                                     header={<div>Phòng ban được quản lý</div>}
                                     bordered
                                     dataSource={dataView.canManageDepartments}
-                                    renderItem={item => (
+                                    renderItem={(item) => (
                                         <List.Item>
-                                            <h4>({item.department}) {item.departmentName}</h4>
+                                            <h4>
+                                                ({item.department}){' '}
+                                                {item.departmentName}
+                                            </h4>
                                         </List.Item>
                                     )}
                                 />
                             </Row>
-                            <CardFooter className="text-right info" style={{marginTop: 20, marginRight: 20}}>
+                            <CardFooter
+                                className="text-right info"
+                                style={{ marginTop: 20, marginRight: 20 }}
+                            >
                                 {this.renderButton()}
                             </CardFooter>
                         </Col>
@@ -254,7 +307,7 @@ class EmployeePermission extends Component {
 }
 
 const mapStateToProps = (state) => ({
-    data: state.homeReducer.data
+    data: state.homeReducer.data,
 });
 
 export default connect(mapStateToProps, null)(EmployeePermission);
