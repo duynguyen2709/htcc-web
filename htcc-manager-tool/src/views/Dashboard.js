39080e2f6a0cdebf9e7ee97342853315bea0720a
duyna5 <duyna5@vng.com.vn>
2020-07-27 18:16:39 +0700

    finish

--- htcc-manager-tool/src/views/Dashboard.js
+++ htcc-manager-tool/src/views/Dashboard.js
@@ -1,8 +1,20 @@
 import React from 'react';
+
 class Dashboard extends React.Component {
-  render() {
-    return <div className="content"></div>;
-  }
+    render() {
+        return (
+            <div className="content">
+                <div className="table-wrapper tabs-small">
+                    <iframe
+                        src="http://167.179.80.90:5000/public/dashboard/65797047-468b-4034-9a34-129fee0071c0"
+                        frameBorder="0"
+                        width="100%"
+                        height="90%"
+                        allowTransparency
+                    />
+                </div>
+            </div>);
+    }
 }
 
 export default Dashboard;
